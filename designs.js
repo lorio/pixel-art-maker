f02ce4221fdf5c12f9fe401bdd25e70fa45ec0ed
lorio <lori@loriortiz.com>
2018-03-03 13:51:58 -0500

    more edits

--- designs.js
+++ designs.js
@@ -5,15 +5,17 @@ var M = $('#inputHeight').val();
 var N = $('#inputWidth').val();
 //get value of gridHeight and gridWidth
 // When size is submitted by the user, call makeGrid()
+
 function makeGrid() {
-  var tbl = document.getElementById('#pixelCanvas');
+  var tbl = document.getElementById('table id="pixelCanvas"');
+  var tbody = document.createElement('tbody');
   for (var i = 0; i <= M; i++){
     var newRow = tbl.insertRow(0);
     for (var j = 0; j <= N; j++) {
       var newCell = newRow.insertCell(0);
+      }
     }
-  }
-  tbl.appendTo(tblBody); 
+    tbl.appendChild(tbody);
   };
   $('#sizePicker').submit(function (evt) {
     evt.preventDefault()
