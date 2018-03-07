e50dbfce9e349dcf63312e7fe3fb2780ba143a1f
lorio <lori@loriortiz.com>
2018-03-07 12:40:04 -0500

    Add jQuery edit vars

--- designs.js
+++ designs.js
@@ -4,33 +4,33 @@ var color = $('#colorPicker').val();
 
 
 // When size is submitted by the user, call makeGrid()
-function makeGrid() {
-var inX, inY, row, col, tbl, tbod;
-//get value of gridHeight and gridWidth
-inY = $('#inputHeight').val();
-inX = $('#inputWidth').val();
 
+var w, h, row, col, tbl;
+//get value of gridHeight and gridWidth
+h = $('#inputHeight').val();
+w = $('#inputWidth').val();
 tbl = $('#pixelCanvas');
-//tbod = $('<tbody></tbody>');
-var tbody =$('<tbody></tbody>');
-tbody.appendTo(tbl);
-/*  
-  for (var i = 0; i <= inY; i++){
-    row = $('tr');
-    tbod.append(row);
+
+
+$('#sizePicker').submit(function makegrid(evt) {
+    evt.preventDefault();  
+    tbl.children().remove(); 
+
+  for (var x = 1; x <= h; x++){
+    row = ('<tr>');
+    tbl.append(row);
  //append columns   
-    for (var j = 0; j <= inX; j++) {
-      col = $('td');
-      row.append(col);
+    for (var y = 1; y <= w; y++) {
+      col = ('<td>');
+      tbl.find('tr').last().append(col);
       }
-    }*/
-  };
-  $('#sizePicker').submit(function (evt) {
-    evt.preventDefault();
-    makeGrid();
-  }); 
+    }
+  });
+
+   
+
     
       // add bg color to cell
-   $('cell').click(function(evt){
-       $(evt.target).css('background', color)
-      });
+ //  $('cell').click(function(evt){
+  //     $(evt.target).css('background', color)
+ //     });
